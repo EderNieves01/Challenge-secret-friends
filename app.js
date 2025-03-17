5d0c9e75d2a7e202d45e0a2cfddc97962f7e022d
EderNieves01 <edernieves09@gmail.com>
2025-03-16 21:49:31 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let btnAddFriends = document.querySelector(".button-add");
+
+ btnAddFriends.addEventListener("click", addFriends);
+function addFriends(){
+    console.log('entrando segunda ves')
+}
