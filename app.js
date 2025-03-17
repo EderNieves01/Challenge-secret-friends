e6b3207948afa397a6f2177939a63e30368f111c
EderNieves01 <edernieves09@gmail.com>
2025-03-16 22:02:33 -0400

    Created resetInput methods

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let btnAddFriends = document.querySelector(".button-add");
+let inputFriend = document.getElementById("amigo");
+
+
+let friends = [];
 
  btnAddFriends.addEventListener("click", addFriends);
 function addFriends(){
-    console.log('entrando segunda ves')
+    friends.push(inputFriend.value);
+    resetInput();
+}
+
+function resetInput(){
+    inputFriend.value = "";
 }
