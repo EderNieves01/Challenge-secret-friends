3b83282ce282f0aecbcba51a86dec0eca1df6892
EderNieves01 <edernieves09@gmail.com>
2025-03-16 22:47:48 -0400

    Final Challenge

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let btnAddFriends = document.querySelector(".button-add");
 let inputFriend = document.getElementById("amigo");
-let showFriends = document.getElementById("resultado");
-
+let showFriends = document.getElementById("listaAmigos");
+let btnDraw = document.querySelector(".button-draw");
+let showResult = document.getElementById("resultado")
 let friends = [];
 
  btnAddFriends.addEventListener("click", addFriends);
+ btnDraw.addEventListener("click", drawFriends);
 
 function addFriends(){
-    let inputValue = inputFriend.value;
+    let inputValue = inputFriend.value.trim();
     console.log(inputValue)
     if(inputValue){
         friends.push(inputValue);
@@ -20,6 +22,20 @@ function addFriends(){
         return;
     }
 
+}
+
+function drawFriends(){
+
+    if (friends.length === 0) {
+        alert("Debes agregar amigos antes de sortear.");
+        return;
+    }
+
+    let randomFriend = friends[Math.floor(Math.random() * friends.length)];
+    showResult.innerHTML = `<li> EL amigo secreto es: ${randomFriend} </li>`;
+
+    friends = [];
+    showFriends.innerHTML = "";
 }
 
 function showFriendsList(){
